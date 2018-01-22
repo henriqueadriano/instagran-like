50d18afbe6dd2179bfe193633fc637c1ae23c6a7
henrique.adriano@aspen.edu <henrique.adriano@aspen.edu>
2018-01-22 11:52:40 -0400

    insert missing modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AllPostsComponent } from './all-posts/all-posts.component';
+import { FollowingComponent } from './following/following.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { MyPostsComponent } from './my-posts/my-posts.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { LoginComponent } from './auth/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AllPostsComponent,
+    FollowingComponent,
+    FavoritesComponent,
+    MyPostsComponent,
+    SignUpComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
